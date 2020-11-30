3386a1c67c306893de07dfa9b3b9feec8b9be77a
Thiago Thomaz <thiago.thomaz@gmail.com>
2020-11-29 23:02:17 -0300

    atualizando para verificacao

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 app.get('/',(request, response)=> {
     return response.json({message: 'Servidor está funcionado e OK'})
+})
+
+app.get('/atualizou',(request, response)=> {
+    return response.json({message: 'Atualizou!!!'})
+})
+
+app.get('/test',(request, response)=> {
+    const {name, date} = request.body;
+    return response.json({name, date})
 })
 
 app.listen(3333)
